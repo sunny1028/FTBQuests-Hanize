f5374041dc7d7183ba73a188fecfd382e749a9bd
孙波 <sunbo22@126.com>
2025-03-08 17:59:43 +0800

    update: 创建翻译服务

---
+++ FTBQHConsole/Translates/TranslateService.cs
@@ -0,0 +1,30 @@
+﻿using FTBQHConsole.Models;
+
+namespace FTBQHConsole.Translates;
+
+/// <summary>
+/// 翻译服务
+/// </summary>
+public class TranslateService
+{
+    /// <summary>
+    /// 执行翻译
+    /// </summary>
+    /// <param name="request"></param>
+    public void Execute(GetRootDirResult request)
+    {
+        ConfigHandling(request.ConfigDir);
+    }
+
+    /// <summary>
+    /// 处理
+    /// </summary>
+    /// <param name="configDir"></param>
+    private void ConfigHandling(DirectoryInfo configDir)
+    {
+        //获取所有文件
+        var snbts = configDir.GetFiles("*.snbt", SearchOption.AllDirectories);
+    
+    }
+
+}
